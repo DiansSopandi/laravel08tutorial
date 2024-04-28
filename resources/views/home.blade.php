a3dcc3b42148372807c10212e920d015e5492ce0
diansopandi <dian.sopandi@nalysa.com>
2024-04-28 13:33:52 +0700

    add model

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.main')
 
-@section('content')    
+@section('content')
     <h2>asguard articles</h2>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,14 +19,14 @@ use Illuminate\Support\Facades\Route;
 // });
 
 
-Route::get('/', function() {
+Route::get('/', function () {
     return view('home', [
         'title' => 'home'
     ]);
 });
 
-Route::get('/about', function() { 
-    return view('about',[
+Route::get('/about', function () {
+    return view('about', [
         'title' => 'about',
         'fullname' => 'guardians asguard',
         'email' => 'guardians.asguard@gmail.com',
@@ -33,52 +34,18 @@ Route::get('/about', function() {
     ]);
 });
 
-Route::get('/posts', function() {
-    $blogPost = [
-        [
-            'title' => 'first title ',
-            'slug' => 'first-title',
-            'author' => 'first author',
-            'post' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur, nesciunt tempora. Dolore asperiores provident, omnis magnam non a! Eum, possimus soluta. Voluptatem minus, iusto reiciendis asperiores eius quibusdam iste tempora molestiae? Libero in corporis, dolorum explicabo repellat, unde minus fugit molestias quis distinctio odio accusantium deserunt. Vero ratione nemo aliquam.'
-        ],
-        [
-            'title' => 'second title',
-            'slug' => 'second-title',
-            'author' => 'second author',
-            'post' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea unde architecto reiciendis et esse perspiciatis nemo voluptatibus. Doloremque cum, dolores numquam, temporibus expedita aspernatur itaque officia beatae ipsa eum nam. In harum laboriosam laudantium repellendus maiores quia odio ipsa adipisci, veniam, quam, blanditiis sunt officiis eum? Iste quis fuga maxime at, distinctio aspernatur repudiandae nesciunt minus tempore harum incidunt deleniti alias impedit, nulla, cumque provident facere? Asperiores sed laudantium ducimus ratione rem ad minima suscipit atque, itaque sit incidunt quidem nobis culpa eius possimus cupiditate! Mollitia nulla ullam ipsum asperiores voluptate tempore praesentium, unde neque? Quibusdam officiis obcaecati alias rem.'
-        ]
-    ];
-    
+Route::get('/posts', function () {
+
     return view('posts', [
         'title' => 'posts',
-        'posts' => $blogPost,
+        'posts' => Post::all(),
     ]);
 });
 
-Route::get('/posts/{slug}', function($slug) {
-    $blogPost = [
-        [
-            'title' => 'first title ',
-            'slug' => 'first-title',
-            'author' => 'first author',
-            'post' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur, nesciunt tempora. Dolore asperiores provident, omnis magnam non a! Eum, possimus soluta. Voluptatem minus, iusto reiciendis asperiores eius quibusdam iste tempora molestiae? Libero in corporis, dolorum explicabo repellat, unde minus fugit molestias quis distinctio odio accusantium deserunt. Vero ratione nemo aliquam.'
-        ],
-        [
-            'title' => 'second title',
-            'slug' => 'second-title',
-            'author' => 'second author',
-            'post' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea unde architecto reiciendis et esse perspiciatis nemo voluptatibus. Doloremque cum, dolores numquam, temporibus expedita aspernatur itaque officia beatae ipsa eum nam. In harum laboriosam laudantium repellendus maiores quia odio ipsa adipisci, veniam, quam, blanditiis sunt officiis eum? Iste quis fuga maxime at, distinctio aspernatur repudiandae nesciunt minus tempore harum incidunt deleniti alias impedit, nulla, cumque provident facere? Asperiores sed laudantium ducimus ratione rem ad minima suscipit atque, itaque sit incidunt quidem nobis culpa eius possimus cupiditate! Mollitia nulla ullam ipsum asperiores voluptate tempore praesentium, unde neque? Quibusdam officiis obcaecati alias rem.'
-        ]
-    ];
-    
-    $selectedPost = [];
-    foreach ($blogPost as $post) {
-        if($post['slug'] === $slug) {
-            $selectedPost = $post;
-        }
-    }
+Route::get('/posts/{slug}', function ($slug) {
+
     return view('post', [
         'title' => 'single post',
-        'post' => $selectedPost,
+        'post' => Post::find($slug),
     ]);
 });
